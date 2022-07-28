e30a1ec4d3ba8df8c453666e1ea5b31a4603d0a8
afermanx <alexjferman@gmail.com>
2022-07-28 17:47:18 -0400

    connection

--- src/main/java/com/digitalinnovationone/springbootconfig/DBConfiguration.java
+++ src/main/java/com/digitalinnovationone/springbootconfig/DBConfiguration.java
@@ -20,7 +20,7 @@ public class DBConfiguration {
     @Profile("dev")
     @Bean
     public String testDatabaseConnection() {
-        System.out.println("DB conection for - H2");
+        System.out.println("DB connection for - H2");
         System.out.println("Driver Class Name: " + driverClassName);
         System.out.println("URL: " + url);
         System.out.println("DB conection for - H2");
